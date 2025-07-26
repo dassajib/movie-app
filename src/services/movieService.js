14f1834f2dc57677695b2d148080fd6af623c3ea
dassajib <sajibdas.sd@gmail.com>
2025-07-26 13:39:36 +0600

    cache busting param

--- src/services/movieService.js
+++ src/services/movieService.js
@@ -12,33 +12,21 @@ const axiosInstance = axios.create({
 
 export const fetchPopularMovies = async () => {
   const res = await axiosInstance.get('/movie/popular', {
-    params: { api_key: API_KEY },
-    headers: {
-      'Cache-Control': 'no-cache',
-      Pragma: 'no-cache',
-    },
+    params: { api_key: API_KEY, t: Date.now() }, // cache busting param
   })
   return res.data.results
 }
 
 export const fetchTrendingMovies = async () => {
   const res = await axiosInstance.get('/trending/movie/day', {
-    params: { api_key: API_KEY },
-    headers: {
-      'Cache-Control': 'no-cache',
-      Pragma: 'no-cache',
-    },
+    params: { api_key: API_KEY, t: Date.now() }, // cache busting param
   })
   return res.data.results
 }
 
 export const searchMovies = async (query) => {
   const res = await axiosInstance.get('/search/movie', {
-    params: { api_key: API_KEY, query },
-    headers: {
-      'Cache-Control': 'no-cache',
-      Pragma: 'no-cache',
-    },
+    params: { api_key: API_KEY, query, t: Date.now() }, // cache busting param
   })
   return res.data.results
 }
